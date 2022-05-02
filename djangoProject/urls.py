1739655b88bbd9fc92b60285ed3ee1d361e448cf
blankxx <Shinima16>
2022-05-02 17:04:33 +0200

    creating route for password reset

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,9 +20,6 @@ import search.urls
 from users import views as users_views
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 urlpatterns = [
     path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
@@ -37,5 +34,31 @@ urlpatterns = [
         "logout/",
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout",
+    ),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="password_reset.html"), name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
 ]
