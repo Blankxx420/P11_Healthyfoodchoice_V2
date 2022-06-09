f4b68a87b6f834c3b5d8c2b756357a2275edad42
blankxx <Shinima16>
2022-06-09 10:36:44 +0200

    refacto test test_views.py for test_substitutes_pages

--- search/test/test_views.py
+++ search/test/test_views.py
@@ -45,9 +45,12 @@ class TestViews(TestCase):
 
     def test_substitutes_page(self):
         response = self.client.get(self.url_substitutes)
-
+        Product.objects.create(id=502, name="Diet nature digestive sin azúcares", nutrition_score="B")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "search/substitutes.html")
+        self.assertEqual(response.context["product"], {'product': '<Product: Nutella biscuits, E>'})
+        self.assertEqual(response.context["substitutes"], {'substitute': '<Product: Diet nature digestive sin azúcares, B>'})
+
 
     def test_save_substitute_as_favorite_success(self):
         self.client.force_login(self.user)
